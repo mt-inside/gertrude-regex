833664b6264eabecf3bafe8ad8285679f48952cf
Matt Turner <git@mt165.co.uk>
2024-01-22 10:10:22 +0000

    Add justfile tools-install target

--- Justfile
+++ Justfile
@@ -1,2 +1,5 @@
+tools-install:
+	rustup target add wasm32-unknown-unknown
+
 build:
 	cargo build --target wasm32-unknown-unknown
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use std::borrow::Cow;
 
 pub struct Plugin;
 
+// TODO: work back through the messages and apply to the first one that matches. Update "didn't
+// apply" to include how many you tried
 impl plugin::Plugin for Plugin {
     fn handle_privmsg(msgs: Vec<String>) -> Option<String> {
         if msgs.len() < 2 {
